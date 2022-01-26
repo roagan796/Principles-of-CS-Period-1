d05c191fbea93738260383d8dc4e608b1ec1cdeb
Roagan Wolf <98339071+roagan796@users.noreply.github.com>
2022-01-26 16:49:26 +0000

    Add all the code to the Mad Libs project.

--- Wolf_Project1MadLibs.py
+++ Wolf_Project1MadLibs.py
@@ -1,1 +1,20 @@
-print("Mad\nLibs")
+#Introduction
+print("Welcome to Mad Libs!\nI will be asking a series of questions that will fill in the\nblanks of a story, creating an even better one! You will be\nanswering these questions with different parts of speech.")
+
+#Asking for the words to fill in the blanks
+properNoun1 = input("Give a proper noun: ")
+properNoun2 = input("Give another proper noun: ")
+adjective = input("Give an adjective: ")
+noun1 = input("Give a singular noun: ")
+properNoun3 = input("Give another proper noun: ")
+verb1 = input("Give a verb: ")
+pronoun = input("Give a pronoun: ")
+verb2 = input("Give another verb: ")
+noun2 = input("Give a noun: ")
+interjection = input("Give an interjection: ")
+adverb = input("Give me an adverb: ")
+noun3 = input("Give me a noun: ")
+noun4 = input("Give me another noun: ")
+
+#Final result
+print(f"In the city of {properNoun1} there is a house owned by {properNoun2}. {properNoun2} was very {adjective} and had a {noun1} named {properNoun3}. {properNoun2} started the day by {verb1}. {properNoun2} would do this for hours until {pronoun} felt like {verb2}. While {verb2}, {properNoun2} would crash into a {noun2}. {interjection} {properNoun2} shouted as {pronoun} {adverb} ran to the {noun3} to get a {noun4} to help with the injury.")
