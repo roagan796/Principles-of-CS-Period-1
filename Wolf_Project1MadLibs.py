4b9c3ecabf3104d962e11d68d18052de9b9dd15c
Roagan Wolf <98339071+roagan796@users.noreply.github.com>
2022-01-27 16:23:15 +0000

    Changed noun2 to a plural noun.

--- Wolf_Project1MadLibs.py
+++ Wolf_Project1MadLibs.py
@@ -12,7 +12,7 @@ properNoun3 = input("Give another proper noun: ")
 verb1 = input("Give a verb: ")
 pronoun = input("Give a pronoun: ")
 verb2 = input("Give another verb: ")
-noun2 = input("Give a noun: ")
+noun2 = input("Give a plural noun: ")
 interjection = input("Give an interjection: ")
 adverb = input("Give an adverb: ")
 noun3 = input("Give a noun: ")
@@ -20,5 +20,5 @@ noun4 = input("Give another noun: ")
 
 #Final result
 print(
-    f"\nIn the city of {properNoun1} there is a house owned by {properNoun2}. {properNoun2} was very {adjective} and had a {noun1} named {properNoun3}. {properNoun2} started the day by {verb1}. {properNoun2} would do this for hours until {pronoun} felt like {verb2}. While {verb2}, {properNoun2} would crash into a {noun2}. {interjection} {properNoun2} shouted as {pronoun} {adverb} ran to the {noun3} to get a {noun4} to help with the injury."
+    f"\nIn the city of {properNoun1} there is a house owned by {properNoun2}. {properNoun2} was very {adjective} and had a {noun1} named {properNoun3}. {properNoun2} started the day by {verb1}. {properNoun2} would do this for hours until {pronoun} felt like {verb2}. While {verb2}, {properNoun2} would crash into some {noun2}. {interjection} {properNoun2} shouted as {pronoun} {adverb} ran to the {noun3} to get a {noun4} to help with the injury."
 )
