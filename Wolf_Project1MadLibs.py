fa6f10e5710311287d023b65053919ff7d6a38ec
Roagan Wolf <98339071+roagan796@users.noreply.github.com>
2022-01-27 16:08:06 +0000

    Added escape characters.

--- Wolf_Project1MadLibs.py
+++ Wolf_Project1MadLibs.py
@@ -1,5 +1,7 @@
 #Introduction
-print("Welcome to Mad Libs!\nI will be asking a series of questions that will fill in the\nblanks of a story, creating an even better one! You will be\nanswering these questions with different parts of speech.")
+print(
+    "Welcome to Mad Libs!\nI will be asking a series of questions that will fill in the\nblanks of a story, creating an even better one! You will be\nanswering these questions with different parts of speech.\n"
+)
 
 #Asking for the words to fill in the blanks
 properNoun1 = input("Give a proper noun: ")
@@ -12,9 +14,11 @@ pronoun = input("Give a pronoun: ")
 verb2 = input("Give another verb: ")
 noun2 = input("Give a noun: ")
 interjection = input("Give an interjection: ")
-adverb = input("Give me an adverb: ")
-noun3 = input("Give me a noun: ")
-noun4 = input("Give me another noun: ")
+adverb = input("Give an adverb: ")
+noun3 = input("Give a noun: ")
+noun4 = input("Give another noun: ")
 
 #Final result
-print(f"In the city of {properNoun1} there is a house owned by {properNoun2}. {properNoun2} was very {adjective} and had a {noun1} named {properNoun3}. {properNoun2} started the day by {verb1}. {properNoun2} would do this for hours until {pronoun} felt like {verb2}. While {verb2}, {properNoun2} would crash into a {noun2}. {interjection} {properNoun2} shouted as {pronoun} {adverb} ran to the {noun3} to get a {noun4} to help with the injury.")
+print(
+    f"\nIn the city of {properNoun1} there is a house owned by {properNoun2}. {properNoun2} was very {adjective} and had a {noun1} named {properNoun3}. {properNoun2} started the day by {verb1}. {properNoun2} would do this for hours until {pronoun} felt like {verb2}. While {verb2}, {properNoun2} would crash into a {noun2}. {interjection} {properNoun2} shouted as {pronoun} {adverb} ran to the {noun3} to get a {noun4} to help with the injury."
+)
